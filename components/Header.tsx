2da322ebfcee2ca8a4310cca472231aa132e7077
Akshit <akshitgautam42@gmail.com>
2024-06-10 23:41:27 +0530

    Videos List added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,37 @@
 import { UserButton, SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 import VideoIcon from "@/components/Icons/VideoIcon";
+import { Button } from "./ui/button";
 
-const Header = () => (
-  <header className="bg-gray-900 text-white py-6 px-4 md:px-8">
-    <div className="container mx-auto flex items-center justify-between">
-      <div className="flex items-center">
-        <VideoIcon className="h-8 w-8 mr-2" />
-        <h1 className="text-2xl font-bold">Video Generator</h1>
+const Header = () => {
+  const router = useRouter();
+
+  return (
+    <header className="bg-gray-900 text-white py-6 px-4 md:px-8">
+      <div className="container mx-auto flex items-center justify-between">
+        <div className="flex items-center">
+          <VideoIcon className="h-8 w-8 mr-2" />
+          <h1 className="text-2xl font-bold">Video Generator</h1>
+        </div>
+        <nav className="flex justify-center">
+          <SignedIn>
+            <Button
+              onClick={() => router.push("/videos-list")}
+              className="bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded mr-4 h-8"
+            >
+              See All Videos
+            </Button>
+            <div className="ml-4">
+            <UserButton />
+            </div>
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+        </nav>
       </div>
-      <nav>
-        <SignedIn>
-        <UserButton />
-        </SignedIn>
-      </nav>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
 export default Header;
